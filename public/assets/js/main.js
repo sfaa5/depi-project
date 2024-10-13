317aea77750be293882aa89f1f10fc144168b81a
sfaa <ym6772003@gmail.com>
2024-10-13 04:38:03 -0700

    home-specialized

---
+++ public/assets/js/main.js
@@ -0,0 +1,7 @@
+let nav = document.querySelector(".bottem-nav");
+
+let navButton = document.querySelector(".nav-btn");
+
+navButton.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
---
+++ src/pages/Home.jsx
@@ -0,0 +1,117 @@
+
+
+
+const Home = () => {
+  return (
+    <>
+    <div class="landing">
+    <div class="container">
+      <div class="content">
+        <h4>Best Medical Clinic</h4>
+        <div>
+          <b class="blue-color">Bringing Health</b>
+          <b>To Life For The Whole Family...</b>
+        </div>
+
+        <button class="btn btn-primary">Get Appoinments »</button>
+      </div>
+    </div>
+  </div>
+
+  <div class="specialized">
+
+
+
+    <div class="container">
+
+
+      <div class="text-section ">
+        <p>We Offer Specialized</p>
+        <h1>Orthopedics To Meet Your Needs</h1>
+      </div>
+
+      <div class="specialisations">
+
+
+
+
+        <div class="card">
+          <img src="./assets/images/home/medical-symbol.png" alt=""/>
+          <div class="card-body">
+
+            <b class="card-title fs-3 fw-bold m-2 ">
+              Medical Treatment
+            </b>
+
+            <p class="card-text m-2 ">
+              Lorem ipsum dolor sit amete
+              consectetur adipisicing elite.
+              vlote optio animi?
+            </p>
+
+            <span class="card-text fw-bold fs-5 m-2">Read More »</span>
+          </div>
+        </div>
+
+
+        <div class="card">
+          <img src="./assets/images/home/delivery-truck.png" alt=""/>
+          <div class="card-body">
+
+            <b class="card-title fs-3 fw-bold m-2">
+              Medical Treatment
+            </b>
+            <p class="card-text m-2 ">
+              Lorem ipsum dolor sit amete
+              consectetur adipisicing elite.
+              vlote optio animi?
+            </p>
+
+            <span class="card-text fw-bold fs-5 m-2">Read More »</span>
+          </div>
+        </div>
+
+        <div class="card">
+          <img src="./assets/images/home/hospital-building.png" alt=""/>
+          <div class="card-body">
+
+            <b class="card-title fs-3 fw-bold m-2">
+              Medical Treatment
+            </b>
+
+            <p class="card-text m-2 ">
+              Lorem ipsum dolor sit amete
+              consectetur adipisicing elite.
+              vlote optio animi?
+            </p>
+            <span class="card-text fw-bold fs-5 m-2">Read More »</span>
+          </div>
+        </div>
+
+        <div class="card">
+          <img src="./assets/images/home/doctor.png" alt=""/>
+          <div class="card-body">
+
+            <b class="card-title fs-3 fw-bold m-2">
+              Medical Treatment
+            </b>
+            <p class="card-text m-2 ">
+              Lorem ipsum dolor sit amete
+              consectetur adipisicing elite.
+              vlote optio animi?
+            </p>
+            <span class="card-text fw-bold fs-5 m-2">Read More »</span>
+          </div>
+        </div>
+
+
+</div>
+      </div>
+    </div>
+
+</>
+
+  )
+}
+
+export default Home
