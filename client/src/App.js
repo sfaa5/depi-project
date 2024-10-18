83dccec6d043dc1b9dbe9c01113e5066c4c14124
sfaa <ym6772003@gmail.com>
2024-10-18 10:31:06 -0700

    add-contact-us

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header';
 import Home from './pages/home/Home';
 import Blog from './pages/blogs/Blog'
+import Contact from './pages/contactUs/contactUs'
 import "./style/all.min.css";
 import  "./style/main.css";
 import { BrowserRouter,  Route, Routes } from "react-router-dom";
@@ -13,11 +14,13 @@ function App() {
     <div className="App">
   <Header/>
 
+  <BrowserRouter>
   <Routes> 
     <Route path="/" element={  <Home/>}  />
    <Route path="Bolg" element={<Blog/>} />
-  
+   <Route path="contact-Us" element={<Contact/>} />
   </Routes>
+  </BrowserRouter>
 
 
     </div>
