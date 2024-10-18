a45962410eb1786770bb5ab34cdd6dc736a6d339
sfaa <ym6772003@gmail.com>
2024-10-18 10:34:39 -0700

    add-services

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Home from './pages/home/Home';
 import Blog from './pages/blogs/Blog'
 import Contact from './pages/contactUs/contactUs'
+import Service from './pages/services/ServiceSection'
 import "./style/all.min.css";
 import  "./style/main.css";
 import { BrowserRouter,  Route, Routes } from "react-router-dom";
@@ -17,8 +18,10 @@ function App() {
   <BrowserRouter>
   <Routes> 
     <Route path="/" element={  <Home/>}  />
+    <Route path="Services" element={<Service/>} />
    <Route path="Bolg" element={<Blog/>} />
    <Route path="contact-Us" element={<Contact/>} />
+   
   </Routes>
   </BrowserRouter>
 
