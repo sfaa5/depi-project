74b20555af020d31e9b09852adb0b5471518c558
sfaa <ym6772003@gmail.com>
2024-10-18 09:57:24 -0700

    add-router-blog-bage

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,23 @@
 import './App.css';
 import Header from './components/Header';
 import Home from './pages/home/Home';
-import "./style/all.min.css"
-
-import  "./style/main.css"
+import Blog from './pages/blogs/Blog'
+import "./style/all.min.css";
+import  "./style/main.css";
+import { BrowserRouter,  Route, Routes } from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
   <Header/>
-  <Home/>
+
+  <Routes> 
+    <Route path="/" element={  <Home/>}  />
+   <Route path="Bolg" element={<Blog/>} />
+  
+  </Routes>
+
 
     </div>
   );
