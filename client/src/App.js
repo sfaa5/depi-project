ec5797810af7f315d87ba0b2523f4420c35b1287
sfaa <ym6772003@gmail.com>
2024-10-18 10:47:13 -0700

    heroSection

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,26 @@
 
 import './App.css';
 import Header from './components/Header';
+import HeroSection from './components/HeroSection'
 import Home from './pages/home/Home';
 import Blog from './pages/blogs/Blog'
 import Contact from './pages/contactUs/contactUs'
 import Service from './pages/services/ServiceSection'
+
 import "./style/all.min.css";
 import  "./style/main.css";
-import { BrowserRouter,  Route, Routes } from "react-router-dom";
+import { BrowserRouter,  Route, Routes,useLocation } from "react-router-dom";
 
 
 function App() {
+const location = useLocation()
+
   return (
     <div className="App">
   <Header/>
 
-  <BrowserRouter>
+  {location.pathname!=="/" && <HeroSection setChange={location.pathname}/>}
+
   <Routes> 
     <Route path="/" element={  <Home/>}  />
     <Route path="Services" element={<Service/>} />
@@ -23,11 +28,19 @@ function App() {
    <Route path="contact-Us" element={<Contact/>} />
    
   </Routes>
-  </BrowserRouter>
+
 
 
     </div>
   );
 }
 
-export default App;
+export default function AppWrapper() {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+}
+
+
