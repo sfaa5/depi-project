8c357a5861028fed7f907ae9ae4ae5d54147870a
sfaa <ym6772003@gmail.com>
2024-10-23 10:14:24 -0700

    Best Centers

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -10,6 +10,7 @@ import Appointment from "./Appointment";
 import { motion } from "framer-motion";
 import Landing from "./supComponent/Landing";
 import Specialized from "./supComponent/Specialized";
+import BestCenters from "./supComponent/BestCenters";
 
 const Home = () => {
   const [isVisible, setIsVisible] = useState(false);
@@ -25,46 +26,12 @@ const Home = () => {
   const dispatch = useDispatch();
   const { doctors, status, error } = useSelector((state) => state.doctors);
 
-
-// useEffect(()=>{
-//     // Dispatch action to fetch doctors
-//     dispatch(fetchDoctors());
-// },[dispatch])
-
-
-useEffect(() => {
-
-  
-  // Create and set up the IntersectionObserver
-  const observer = new IntersectionObserver(
-    ([entry]) => {
-      if (entry.isIntersecting) {
-        setIsVisible(true); // Set visibility to true when section is in view
-        observer.disconnect(); // Stop observing once the section is visible
-      }
-    },
-    {
-      threshold: 0.1, // Adjust threshold as needed
-    }
-  );
-
-  // Check if sectionRef is defined, and observe it
-  if (sectionRef.current) {
-    observer.observe(sectionRef.current);
-  }
-
-  // Cleanup function to stop observing when the component unmounts
-  return () => {
-    if (sectionRef.current) {
-      observer.unobserve(sectionRef.current);
-    }
-  };
-}, [sectionRef]);
-
+  // useEffect(()=>{
+  //     // Dispatch action to fetch doctors
+  //     dispatch(fetchDoctors());
+  // },[dispatch])
 
   useEffect(() => {
-
-  
     // Create and set up the IntersectionObserver
     const observer = new IntersectionObserver(
       ([entry]) => {
@@ -77,12 +44,12 @@ useEffect(() => {
         threshold: 0.1, // Adjust threshold as needed
       }
     );
-  
+
     // Check if sectionRef is defined, and observe it
     if (sectionRef.current) {
       observer.observe(sectionRef.current);
     }
-  
+
     // Cleanup function to stop observing when the component unmounts
     return () => {
       if (sectionRef.current) {
@@ -91,6 +58,32 @@ useEffect(() => {
     };
   }, [sectionRef]);
 
+  useEffect(() => {
+    // Create and set up the IntersectionObserver
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true); // Set visibility to true when section is in view
+          observer.disconnect(); // Stop observing once the section is visible
+        }
+      },
+      {
+        threshold: 0.1, // Adjust threshold as needed
+      }
+    );
+
+    // Check if sectionRef is defined, and observe it
+    if (sectionRef.current) {
+      observer.observe(sectionRef.current);
+    }
+
+    // Cleanup function to stop observing when the component unmounts
+    return () => {
+      if (sectionRef.current) {
+        observer.unobserve(sectionRef.current);
+      }
+    };
+  }, [sectionRef]);
 
   if (status === "loading") {
     return <div className="animate-pulse">Loading...</div>;
@@ -102,86 +95,11 @@ useEffect(() => {
 
   return (
     <>
+      <Landing />
 
-      <Landing/>
+      <Specialized />
 
-
-
-     <Specialized/>
-
- 
-
-<section className="Best-centers" >
-  <div className="container" ref={sectionRef}>
-    
-    {/* Animate the text */}
-    <div className="text-section">
-      <motion.p
-        initial={{ opacity: 0, x: -100 }}  // Slide in from the left
-        animate={isVisible ?{ opacity: 1, x: 0 }:{}}     // Animate to the original position
-        transition={{ duration: 0.7 }}     // Set the animation duration
-      >
-        we are the
-      </motion.p>
-      <motion.h1
-        initial={{ opacity: 0, x: 100 }}  // Slide in from the right
-        animate={isVisible ?{ opacity: 1, x: 0 }:{}}    // Animate to the original position
-        transition={{ duration: 0.7, delay: 0.2 }}  // Add a delay for staggered effect
-      >
-        best our departments centers
-      </motion.h1>
-    </div>
-    
-    {/* Animate icons with staggered effect */}
-    <div className="icons">
-      {["./assets/images/home/01.png", "./assets/images/home/01.png", "./assets/images/home/03.png", "./assets/images/home/04.png", "./assets/images/home/05.png", "./assets/images/home/06.png", "./assets/images/home/07.png", "./assets/images/home/08.png", "./assets/images/home/09.png", "./assets/images/home/01.png"].map((src, index) => (
-        <motion.img
-          key={index}
-          className="icon"
-          src={src}
-          alt=""
-          initial={{ opacity: 0, y: 50 }} 
-          animate={isVisible ?{ opacity: 1, y: 0 }:{}}  
-          transition={{ duration: 0.5, delay: index * 0.1 }} 
-        />
-      ))}
-    </div>
-
-
-    <div className="speciality-rhinology">
-      <motion.div
-        className="left"
-        initial={{ opacity: 0, x: -300 }}  
-        animate={isVisible ?{ opacity: 1, x: 0 }:{}}    
-        transition={{ duration: 0.7 }}    
-      >
-        <b>speciality rhinology 1</b>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum exercitationem quia debitis sint ex obcaecati dolor iure repellat cum! Debitis placeat accusantium error vitae, ad cum unde alias corrupti iure? Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere voluptate, fugit nobis fugiat officia expedita. Laborum voluptates culpa nostrum. Adipisci assumenda autem et odit inventore natus aperiam nesciunt excepturi vitae!
-        </p>
-        <div className="services">
-          <span> qualified doctors</span>
-          <span> qualified doctors</span>
-          <span> qualified doctors</span>
-          <span> qualified doctors</span>
-          <span> qualified doctors</span>
-          <span> qualified doctors</span>
-        </div>
-        <button className="btn">Appointment Now</button>
-      </motion.div>
-
-      <motion.div
-        className="right"
-        initial={{ opacity: 0, x: 300 }}  
-        animate={isVisible ?{ opacity: 1, x: 0 }:{}}    
-        transition={{ duration: 0.7 }}    
-      >
-
-      </motion.div>
-    </div>
-  </div>
-</section>
-
+      <BestCenters />
 
       <section class="info">
         <div class="container">
--- client/src/pages/home/supComponent/Specialized.js
+++ client/src/pages/home/supComponent/Specialized.js
@@ -2,148 +2,141 @@ import { useEffect, useRef, useState } from "react";
 
 import { motion } from "framer-motion";
 
-
-
 const Specialized = () => {
-    const [isVisible, setIsVisible] = useState(false);
-    const sectionRef = useRef(null);
+  const [isVisible, setIsVisible] = useState(false);
+  const sectionRef = useRef(null);
 
-
-    useEffect(() => {
-
-  
-        // Create and set up the IntersectionObserver
-        const observer = new IntersectionObserver(
-          ([entry]) => {
-            if (entry.isIntersecting) {
-              setIsVisible(true); // Set visibility to true when section is in view
-              observer.disconnect(); // Stop observing once the section is visible
-            }
-          },
-          {
-            threshold: 0.1, // Adjust threshold as needed
-          }
-        );
-      
-        // Check if sectionRef is defined, and observe it
-        if (sectionRef.current) {
-          observer.observe(sectionRef.current);
+  useEffect(() => {
+    // Create and set up the IntersectionObserver
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true); // Set visibility to true when section is in view
+          observer.disconnect(); // Stop observing once the section is visible
         }
-      
-        // Cleanup function to stop observing when the component unmounts
-        return () => {
-          if (sectionRef.current) {
-            observer.unobserve(sectionRef.current);
-          }
-        };
-      }, [sectionRef]);
+      },
+      {
+        threshold: 0.1, // Adjust threshold as needed
+      }
+    );
 
+    // Check if sectionRef is defined, and observe it
+    if (sectionRef.current) {
+      observer.observe(sectionRef.current);
+    }
 
+    // Cleanup function to stop observing when the component unmounts
+    return () => {
+      if (sectionRef.current) {
+        observer.unobserve(sectionRef.current);
+      }
+    };
+  }, [sectionRef]);
 
   return (
     <div className="specialized">
-    <div className="container" ref={sectionRef}>
-    <div className="text-section">
-      <motion.p
-        initial={{  x: 300 }} // Start off-screen to the right
-        animate={isVisible ?{  x: 0} : {}}  // Animate to the original position
-        transition={{ duration: 0.7 }} // Set the animation duration
-      >
-        We Offer Specialized
-      </motion.p>
+      <div className="container" ref={sectionRef}>
+        <div className="text-section">
+          <motion.p
+            initial={{ x: 300 }} // Start off-screen to the right
+            animate={isVisible ? { x: 0 } : {}} // Animate to the original position
+            transition={{ duration: 0.7 }} // Set the animation duration
+          >
+            We Offer Specialized
+          </motion.p>
 
-      <motion.h1
-        initial={{ opacity: 0, x: -300 }} // Start off-screen to the left
-        animate={isVisible ?{ opacity: 1, x: 0} : {}} // Animate to the original position
-        transition={{ duration: 0.7, delay: 0.2 }} // Set the duration and add delay for staggered effect
-      >
-        Orthopedics To Meet Your Needs
-      </motion.h1>
-      </div>
+          <motion.h1
+            initial={{ opacity: 0, x: -300 }} // Start off-screen to the left
+            animate={isVisible ? { opacity: 1, x: 0 } : {}} // Animate to the original position
+            transition={{ duration: 0.7, delay: 0.2 }} // Set the duration and add delay for staggered effect
+          >
+            Orthopedics To Meet Your Needs
+          </motion.h1>
+        </div>
 
-      <div className="specialisations" ref={sectionRef}>
-        {/* First Card Group Coming From the Right */}
-        <motion.div
-          className="card"
-          initial={{ opacity: 0, x: 100 }} // Start from the right
-          animate={isVisible ? { opacity: 1, x: 0 } : {}} // Animate to original position
-          transition={{ duration: 0.5 }}
-        >
-          <img src="./assets/images/home/medical-symbol.png" alt="" />
-          <div className="card-body">
-            <b className="card-title">Medical Treatment</b>
-            <p className="card-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vlo
-              te optio animi?
-            </p>
-            <span className="card-text">Read More »</span>
-          </div>
-        </motion.div>
+        <div className="specialisations" ref={sectionRef}>
+          {/* First Card Group Coming From the Right */}
+          <motion.div
+            className="card"
+            initial={{ opacity: 0, x: 100 }} // Start from the right
+            animate={isVisible ? { opacity: 1, x: 0 } : {}} // Animate to original position
+            transition={{ duration: 0.5 }}
+          >
+            <img src="./assets/images/home/medical-symbol.png" alt="" />
+            <div className="card-body">
+              <b className="card-title">Medical Treatment</b>
+              <p className="card-text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vlo te
+                optio animi?
+              </p>
+              <span className="card-text">Read More »</span>
+            </div>
+          </motion.div>
 
-        <motion.div
-          className="card"
-          initial={{ opacity: 0, x: 100 }} // Start from the right
-          animate={isVisible ? { opacity: 1, x: 0 } : {}} // Animate to original position
-          transition={{ duration: 0.5, delay: 0.2 }} // Add delay for staggered effect
-        >
-          <img
-            src="./assets/images/home/delivery-truck.png"
-            alt="Medical Treatment"
-          />
-          <div className="card-body">
-            <b className="card-title">Emergency Help</b>
-            <p className="card-text m-2">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vlo
-              te optio animi?
-            </p>
-            <span className="card-text">Read More »</span>
-          </div>
-        </motion.div>
+          <motion.div
+            className="card"
+            initial={{ opacity: 0, x: 100 }} // Start from the right
+            animate={isVisible ? { opacity: 1, x: 0 } : {}} // Animate to original position
+            transition={{ duration: 0.5, delay: 0.2 }} // Add delay for staggered effect
+          >
+            <img
+              src="./assets/images/home/delivery-truck.png"
+              alt="Medical Treatment"
+            />
+            <div className="card-body">
+              <b className="card-title">Emergency Help</b>
+              <p className="card-text m-2">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vlo te
+                optio animi?
+              </p>
+              <span className="card-text">Read More »</span>
+            </div>
+          </motion.div>
 
-        {/* Second Card Group Coming From the Left */}
-        <motion.div
-          className="card"
-          initial={{ opacity: 0, x: -100 }} // Start from the left
-          animate={isVisible ? { opacity: 1, x: 0 } : {}} // Animate to original position
-          transition={{ duration: 0.5, delay: 0.4 }} // Add delay for staggered effect
-        >
-          <img
-            src="./assets/images/home/hospital-building.png"
-            alt="Medical Treatment"
-          />
-          <div className="card-body">
-            <b className="card-title">Medical Professionals</b>
-            <p className="card-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vlo
-              te optio animi?
-            </p>
-            <span className="card-text">Read More »</span>
-          </div>
-        </motion.div>
+          {/* Second Card Group Coming From the Left */}
+          <motion.div
+            className="card"
+            initial={{ opacity: 0, x: -100 }} // Start from the left
+            animate={isVisible ? { opacity: 1, x: 0 } : {}} // Animate to original position
+            transition={{ duration: 0.5, delay: 0.4 }} // Add delay for staggered effect
+          >
+            <img
+              src="./assets/images/home/hospital-building.png"
+              alt="Medical Treatment"
+            />
+            <div className="card-body">
+              <b className="card-title">Medical Professionals</b>
+              <p className="card-text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vlo te
+                optio animi?
+              </p>
+              <span className="card-text">Read More »</span>
+            </div>
+          </motion.div>
 
-        <motion.div
-          className="card"
-          initial={{ opacity: 0, x: -100 }} // Start from the left
-          animate={isVisible ? { opacity: 1, x: 0 } : {}} // Animate to original position
-          transition={{ duration: 0.5, delay: 0.6 }} // Add delay for staggered effect
-        >
-          <img
-            src="./assets/images/home/doctor.png"
-            alt="Medical Treatment"
-          />
-          <div className="card-body">
-            <b className="card-title">Qualified Doctors</b>
-            <p className="card-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vlo
-              te optio animi?
-            </p>
-            <span className="card-text">Read More »</span>
-          </div>
-        </motion.div>
+          <motion.div
+            className="card"
+            initial={{ opacity: 0, x: -100 }} // Start from the left
+            animate={isVisible ? { opacity: 1, x: 0 } : {}} // Animate to original position
+            transition={{ duration: 0.5, delay: 0.6 }} // Add delay for staggered effect
+          >
+            <img
+              src="./assets/images/home/doctor.png"
+              alt="Medical Treatment"
+            />
+            <div className="card-body">
+              <b className="card-title">Qualified Doctors</b>
+              <p className="card-text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vlo te
+                optio animi?
+              </p>
+              <span className="card-text">Read More »</span>
+            </div>
+          </motion.div>
+        </div>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default Specialized
+export default Specialized;
